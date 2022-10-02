f0b201645dbbb71653646cdd5adf6f4100654d34
Julia Gnetneva <juliagnetneva@gmail.com>
2022-10-02 21:31:01 +0300

    feat: create content and add styles for Error Page

--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -1,12 +1,39 @@
 import React from "react";
 //
-import { HeroBanner } from "../components";
+
 import ErrorImage from "../assets/hero/hero_banner_error.jpg";
+import { HeadingBold, HeadingOutlined, HeadingRedCursive } from "../components";
 
 export const ErrorPage = () => {
   return (
     <section>
-      <HeroBanner image={ErrorImage} />
+      <div
+        style={{
+          height: "100%",
+          width: "100%",
+          position: "absolute",
+          top: "0",
+          left: "0",
+          right: "0",
+        }}
+      >
+        <img
+          src={ErrorImage}
+          alt="Discover the Planet"
+          style={{
+            height: "100%",
+            width: "100%",
+
+            objectFit: "cover",
+          }}
+        />
+      </div>
+      <div style={{ position: "relative", zIndex: "99" }}>
+        <HeadingBold>
+          Page<HeadingRedCursive> not </HeadingRedCursive>found <br />
+          <HeadingOutlined>404</HeadingOutlined>
+        </HeadingBold>
+      </div>
     </section>
   );
 };
